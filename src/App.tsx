7db683e6693ba6a3ba6b0352e9ce275f37941160
kss2002 <kss2002@users.noreply.github.com>
2026-03-11 15:52:40 +0900

    feat: 로컬 서버에서도 디자인 컴포넌트 예시 할당해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { Badge } from './stories/components/Badge';
+import { Header } from './stories/components/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header
+        user={{ name: 'Jane Doe' }}
+        onLogin={() => {}}
+        onLogout={() => {}}
+        onCreateAccount={() => {}}
+      />
+      <h1 className="text-3xl font-bold p-4">Hello world!</h1>
+      <p className="mt-2 text-gray-600 p-6">Welcome to your Storybook demo.</p>
+      <Badge label="New" variant="success" size="large" />
     </>
   );
 }
